8bf904a917220d8464d7c7c0d6988a523a233f29
Vitor Caetano <vitorcaetano1888@gmail.com>
2025-05-09 09:51:07 -0300

    sla

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   /* config options here */
   // Add transpilePackages to ensure Phaser works correctly
-  transpilePackages: ['phaser']
+  transpilePackages: ['phaser'],
+  // Configure the build output directory
+  output: 'export'
 };
 
 module.exports = nextConfig; 
