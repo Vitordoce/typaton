b287ef86bc6c3dacec8ef09faf679c43d8364db2
Vitor Caetano <vitorcaetano1888@gmail.com>
2025-05-12 00:05:36 -0300

    hello

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   /* config options here */
   // Add transpilePackages to ensure Phaser works correctly
-  transpilePackages: ['phaser']
+  transpilePackages: ['phaser'],
+  // Add output configuration to generate the "out" directory for Vercel deployment
+  output: 'export',
 };
 
 module.exports = nextConfig; 
